bc1e0d2db515c3d2e665b4c042804b8f37f4c8a3
MohamadTdn <mohamadmt1579@gmail.com>
2024-06-28 12:53:03 +0330

    add content component

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Slider from '../Slider/Slider'
 import Filter from '../Filter'
+import Content from '../Content/Content'
 
 export default function Home() {
   return (
     <div className='Home'>
       <Slider/>
       <Filter/>
+      <Content/>
     </div>
   )
 }

caa29eb62ec139ae30702b4ae2335e379b87ebd3
MohamadTdn <mohamadmt1579@gmail.com>
2024-06-28 12:43:55 +0330

    add filter component

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Slider from '../Slider/Slider'
+import Filter from '../Filter'
 
 export default function Home() {
   return (
     <div className='Home'>
       <Slider/>
+      <Filter/>
     </div>
   )
 }

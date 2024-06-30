bf148da443ffff40b61b4a16b51f0db6799bee3a
MohamadTdn <mohamadmt1579@gmail.com>
2024-06-30 13:18:24 +0330

    coding sidebar component

--- src/Components/Sidebar/Sidebar.js
+++ src/Components/Sidebar/Sidebar.js
@@ -1,7 +1,20 @@
 import React from 'react'
+import Button from '@mui/material/Button';
+import TelegramIcon from '@mui/icons-material/Telegram';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import Stack from '@mui/material/Stack';
 
 export default function Sidebar() {
   return (
-    <div>Sidebar</div>
+    <div className='Sidebar'>
+      <Stack direction="row" spacing={2}>
+        <Button color='secondary' style={{margin: '0 10px', fontSize: '20px'}} variant="contained" startIcon={<InstagramIcon />}>
+          اینستاگرام
+        </Button>
+        <Button style={{padding: '10px', fontSize: '20px'}} variant="contained" startIcon={<TelegramIcon />}>
+          تلگرام
+        </Button>
+      </Stack>
+    </div>
   )
 }

3649bfea7646ae90488b5a7235e6d63c80c82ab9
MohamadTdn <mohamadmt1579@gmail.com>
2024-07-02 19:45:51 +0330

    finish static template

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,7 +1,37 @@
 import React from 'react'
+import { Container, Row, Col } from 'react-bootstrap'
+import Sidebar from '../Sidebar/Sidebar'
+import NavMenu from '../NavMenu/NavMenu'
+import Footer from '../Footer/Footer'
+import './Contact.css'
+import InstagramIcon from '@mui/icons-material/Instagram';
+import TelegramIcon from '@mui/icons-material/Telegram';
 
 export default function Contact() {
   return (
-    <div>Contact</div>
+    <div style={{color: '#ffff'}}>
+      <NavMenu/>
+      <Container>
+        <Row>
+          <Col lg={9} style={{padding: '90px 0 0 0'}}>
+            <h4>تماس با ما</h4>
+            <ul>
+              <li>
+                <InstagramIcon/>
+                اینستاگرام
+              </li>
+              <li>
+                <TelegramIcon/>
+                تلگرام
+              </li>
+            </ul>
+          </Col>
+          <Col lg={3} style={{padding: '90px 0 0 0'}}>
+            <Sidebar/>
+          </Col>
+        </Row>
+      </Container>
+      <Footer/>
+    </div>
   )
 }

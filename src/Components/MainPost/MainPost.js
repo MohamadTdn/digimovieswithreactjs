32ade26804222fbec74140975e2c9e4f5204df84
MohamadTdn <mohamadmt1579@gmail.com>
2024-07-05 01:03:56 +0330

    complete main post page

--- src/Components/MainPost/MainPost.js
+++ src/Components/MainPost/MainPost.js
@@ -11,6 +11,7 @@ export default function MainPost() {
 
   const [posts, setPosts] = useState([])
   const params = useParams()
+  const [target, setTarget] = useState()
 
   async function getFromApi() {
     await fetch("https://tedimovie-df041-default-rtdb.firebaseio.com/posts.json", {
@@ -21,6 +22,15 @@ export default function MainPost() {
         setPosts((prevState) => {
           return (prevState = Object.entries(data));
         });
+        let allPosts = Object.entries(data)
+
+        let mainPost = allPosts.find(post => {
+          return post[1].id == params.id
+        })
+
+        if (mainPost != undefined) {
+          setTarget(mainPost)
+        }
       })
       .catch((err) => {
         err = new Error("cant get from api");
@@ -31,19 +41,20 @@ export default function MainPost() {
 
   useEffect(() => {
     getFromApi();
-  });
+  }, []);
 
   return (
     <div style={{ color: "#ffff" }}>
       <NavMenu />
       <Container className="MinPost-Container">
         <Row>
+        {target ? (
               <Col lg={9} style={{ padding: "90px 0 0 0" }}>
                 <div
                   className="Post-background"
                   style={{
                     backgroundImage:
-                      `url()`,
+                      `url(${target[1].Cover})`,
                     height: "100px",
                     width: "700px",
                     backgroundRepeat: "no-repeat",
@@ -53,45 +64,35 @@ export default function MainPost() {
                 <div style={{ padding: "10px 0", display: "flex" }}>
                   <img
                     style={{ width: "250px", height: "350px" }}
-                    src=''
+                    src={target[1].Cover}
                     alt=""
                   />
                   <div style={{ padding: "10px" }}>
-                    <h4>Inception 2010</h4>
+                    <h4>{target[1].title}</h4>
                     <img style={{ width: "30px" }} src="/assets/imdb.png" alt="" />
-                    <span>8.7</span>
+                    <span>{target[1].Imdb}</span>
                     <br />
                     <span>امتیاز منتقدان</span>
                     <span style={{ width: "30px", backgroundColor: "green" }}>
-                      67
+                      {target[1].Metacritic}
                     </span>
-                    <h5>سال تولید : تستی</h5>
-                    <h5>محصول: تستی</h5>
-                    <h5>ژانر: تستی</h5>
-                    <h5>زمان: تستی</h5>
+                    <h5>سال تولید : {target[1].Year}</h5>
+                    <h5>محصول: {target[1].Country}</h5>
+                    <h5>ژانر: {target[1].Genre}</h5>
+                    <h5>زمان: {target[1].Duration} دقیقه</h5>
                     <Button variant="warning">پخش آنلاین</Button>
                     <Button variant="secondary">پخش آنلاین</Button>
                   </div>
                 </div>
                 <p className="Story">
-                  لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با
-                  استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله
-                  در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد
-                  نیاز، و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد،
-                  کتابهای زیادی در شصت و سه درصد گذشته حال و آینده، شناخت فراوان
-                  جامعه و متخصصان را می طلبد، تا با نرم افزارها شناخت بیشتری را برای
-                  طراحان رایانه ای علی الخصوص طراحان خلاقی، و فرهنگ پیشرو در زبان
-                  فارسی ایجاد کرد، در این صورت می توان امید داشت که تمام و دشواری
-                  موجود در ارائه راهکارها، و شرایط سخت تایپ به پایان رسد و زمان مورد
-                  نیاز شامل حروفچینی دستاوردهای اصلی، و جوابگوی سوالات پیوسته اهل
-                  دنیای موجود طراحی اساسا مورد استفاده قرار گیرد.
+                  {target[1].Story}
                 </p>
                 <ul className="data">
-                  <li>زمان: تستی</li>
-                  <li>گروه سنی: تستی</li>
-                  <li>محصول: تستی</li>
-                  <li>سال تولید: تستی</li>
-                  <li>زبان: تستی</li>
+                  <li>زمان: {target[1].Duration} دقیقه</li>
+                  <li>گروه سنی: {target[1].Esrb}</li>
+                  <li>محصول: {target[1].Country}</li>
+                  <li>سال تولید: {target[1].Year}</li>
+                  <li>زبان: {target[1].Country}</li>
                 </ul>
                 <ul className="Crew">
                   <h4>کارگردان</h4>
@@ -103,31 +104,38 @@ export default function MainPost() {
                           height: "50px",
                           borderRadius: "50%",
                         }}
-                        src="/assets/post.jfif"
+                        src={target[1].Director}
                         alt=""
                       />
                     </span>
-                    <span>Christopher Nolan</span>
+                    <span>{target[1].Director}</span>
                   </li>
                   <h4>بازیگران</h4>
-                  <li className="Actors">
-                    <span>
-                      <img
-                        style={{
-                          width: "50px",
-                          height: "50px",
-                          borderRadius: "50%",
-                        }}
-                        src="/assets/post.jfif"
-                        alt=""
-                      />
-                    </span>
-                    <span>leonardo Di Caprio</span>
-                  </li>
+                  {target[1].Actores.map(actor => {
+                    return (
+                      <li className="Actors">
+                        <span>
+                          <img
+                            style={{
+                              width: "50px",
+                              height: "50px",
+                              borderRadius: "50%",
+                            }}
+                            src={actor.Image}
+                            alt=""
+                          />
+                        </span>
+                        <span>{actor.name}</span>
+                      </li>
+                    )
+                  })}
                 </ul>
                 <h4>نظرات</h4>
                 <Comment />
               </Col>
+        ) : (
+          null
+        )}
           <Col lg={3} style={{ padding: "90px 0 0 0" }}>
             <Sidebar />
           </Col>
